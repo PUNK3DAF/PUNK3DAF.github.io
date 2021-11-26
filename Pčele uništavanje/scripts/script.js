794d96f91c872e568b9eb697e87fe70a366cb861
Vld Mrk <vladamark00@gmail.com>
2021-11-26 17:58:33 +0100

    eo ga

--- Pčele uništavanje/scripts/script.js
+++
@@ -1,6 +0,0 @@
-$(".dugme").on("click", function () {
-  if ($(this).text() == "Ponovi!") $(this).text("Spasi pčelu!");
-  else $(this).text("Ponovi!");
-  $("#pcela").toggle(2000);
-  $(".p2").toggle(3000);
-});
